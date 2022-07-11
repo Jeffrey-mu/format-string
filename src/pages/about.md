ae32c5070cac476e8947b0103275997f775ad20c
Jeffrey-mu <1115378579@qq.com>
2022-07-11 22:46:26 +0800

    feat：add stringformat function

--- src/pages/about.md
+++ src/pages/about.md
@@ -1,21 +1,85 @@
----
-title: About
----
+#### 在处理不同问题域名 通过汉字对字符串进行分组
+- 示例
+输入:
+```txt
+AQ-违规：
+codogco.com
+ecouponsx.com
+beaar2game.com
+shoprebates.site
+gameing.site
+coomanga.com
+tattooshow.top
+woniuzx.com
+lilipoor.com
+seconess.com
+tritang.com
+shenlizer.com
+sigrina.com
+mengaic.com
+senpertun.com
+tallegame.com
+leleigame.com
+dgingames.com
+nowlegame.com
+pklfinance.com
+mniagame.com
+higame.app
+aborgame.com
+cruitygames.com
+evbygames.com
+effervgame.com
+ephegame.com
+enteryuer.com
+jobsaimee.com
+foodaimee.com
+oriehorse.com
+suitabfinance.com
 
-<div class="text-center">
-  <!-- You can use Vue components inside markdown -->
-  <div i-carbon-dicom-overlay class="text-4xl -mb-6 m-auto" />
-  <h3>About</h3>
-</div>
 
-[Vitesse](https://github.com/antfu/vitesse) is an opinionated [Vite](https://github.com/vitejs/vite) starter template made by [@antfu](https://github.com/antfu) for mocking apps swiftly. With **file-based routing**, **components auto importing**, **markdown support**, I18n, PWA and uses **UnoCSS** for styling and icons.
+Start.io违规：
+blackjackm.com
+itsoftcore.com
+zeustarot.com
+itsurfing.com
 
-```js
-// syntax highlighting example
-function vitesse() {
-  const foo = 'bar'
-  console.log(foo)
-}
+
+AQ-缺ads.txt：
+game3h.com
+heroscloud.com
+granfinance.com
+3gsite.com
+izhuolu.com
+picahold.com
+brawsite.com
+yepwish.com
+
+Start.io-缺ads.txt:
+ciopfinance.com
+genthorse.com
+
+
+AQ-已下线，无法访问:
+dabogame.com
+gbapplet.com
+lymini.com
+fineased.com
+withomee.com
+dinghorse.cn
+mockhorse.com
+axiohorse.com
+dvdaimee.com
+nginhorse.com
+errohorse.com
+convefinance.com
+indiragoop.com
+woulfence.com
+
+Start.IO-已下线，无法访问
+begoniafinane.com
+birdsfinance.com
+okecfinance.com
+acceitnews.com
 ```
-
-Check out the [GitHub repo](https://github.com/antfu/vitesse) for more details.
+输出:
+以汉字分类的json对象
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,57 +1,73 @@
 <script setup lang="ts">
-const user = useUserStore()
-const name = $ref(user.savedName)
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-
-const { t } = useI18n()
+import { JsonViewer } from 'vue3-json-viewer'
+import 'vue3-json-viewer/dist/index.css'
+const input = ref<string>('')
+const output = computed(() => {
+  if (!input.value)
+    return ''
+  const dataArray = input.value
+    .trim()
+    .replaceAll('.com', '')
+    .replaceAll('.app', '')
+    .replaceAll('.cn', '')
+    .replaceAll('.top', '')
+    .replaceAll('.site', '')
+    .replaceAll('.cn', '')
+    .split('\n')
+    .filter(Boolean)
+  const includeChineseCharacters: number[] = []
+  const returnValue = []
+  function format(index = 0): void {
+    const reg = /.*[\u4E00-\u9FA5].*/
+    const hz: number = dataArray.slice(index).findIndex((item) => {
+      return reg.test(item)
+    })
+    if (hz !== -1) {
+      includeChineseCharacters.push(hz + index)
+      format(hz + 1 + index)
+    }
+  }
+  format()
+  const len: number = includeChineseCharacters.length
+  includeChineseCharacters.reduce((pre, next) => {
+    if (next || len === 1) {
+      returnValue.push({
+        name: dataArray[pre],
+        data: dataArray.slice(pre + 1, len === 1 ? Infinity : next),
+      })
+    }
+    return (pre = next)
+  }, 0)
+  returnValue.push({
+    name: dataArray[includeChineseCharacters[len - 1]],
+    data: dataArray.slice(includeChineseCharacters[len - 1], Infinity),
+  })
+  return returnValue
+})
 </script>
 
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
+  <div flex h80vh>
+    <div flex="1">
+      <textarea
+        v-model="input"
+        class="textarea"
+        border="1 rd"
+        cols="30"
+        rows="10"
+        placeholder="input"
+      />
     </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      p="x4 y2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+    <div flex="1">
+      <JsonViewer
+        :value="output"
+        copyable
+        preview-mode
+        class="textarea"
+        overflow-auto
+        border="1 rd"
+      />
+      <!-- <textarea id="" v-model="output" border cols="30" rows="10" placeholder="output" disabled /> -->
     </div>
   </div>
 </template>
@@ -60,3 +76,28 @@ const { t } = useI18n()
 meta:
   layout: home
 </route>
+
+<style>
+.textarea {
+  position: relative;
+  outline: none;
+  padding: 10px !important;
+  width: 80%;
+  height: 100%;
+  background: none;
+}
+ .dark .jv-container.jv-light,
+ .dark .jv-array,
+ .dark .jv-object,
+ .dark .jv-key {
+   background: #111 !important;
+   color: aliceblue !important;
+ }
+ .dark .jv-ellipsis {
+   background: #111 !important;
+   color: aliceblue !important;
+ }
+ .jv-tooltip .right {
+  position:absolute;
+ }
+</style>
