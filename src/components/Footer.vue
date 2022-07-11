91b764b6ad647c806feb5d76fa662f84ede32e76
Jeffrey-mu <1115378579@qq.com>
2022-07-11 22:50:44 +0800

    docs: update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,7 +16,7 @@ const toggleLocales = () => {
     <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
       <div i-carbon-dicom-overlay />
     </RouterLink>
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/Jeffrey-mu/format-string" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
